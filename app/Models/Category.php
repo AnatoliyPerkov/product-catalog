58802dcb84468f0b529c4bc742d7fe07c0d66355
AnatoliyPerkov <perkovdesign@gmail>
2025-06-05 11:08:45 +0300

    зміни в модель Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,10 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'slug'];
+    protected $fillable = ['external_id', 'name', 'slug', 'parent_id'];
 
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
     }
 }
